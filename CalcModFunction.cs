e4b7cac76f1a584528a68b04e6a134614e693e27
Chandrea Patterson <146553317+chandreapatterson@users.noreply.github.com>
2025-05-28 00:53:31 -0400

    Initial commit

---
+++ CalcModFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPlayground
+{
+    class CalcModFunction
+    {
+        public static void calcMod(double calcNumMod)
+        {
+
+            // Check if input is divisible by 3
+            if (calcNumMod % 3 is 0)
+            {
+                // Output Fizz if input divisible by 3
+                Console.WriteLine("Fizz!");
+            }
+
+            // Check if input is divisible by 5
+            if (calcNumMod % 5 is 0)
+            {
+                // Output Buzz if input divisible by 5
+                Console.WriteLine("Buzz!");
+            }
+
+            // Check if not divisible by 3 or 5
+            if ((!(calcNumMod % 3 is 0)) & (!(calcNumMod % 5 is 0)))
+            {
+                // Output message if not divisible by 3 or 5
+                Console.WriteLine("Not divisible by 3 or 5");
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            double numInput = ConvertToNumFunction.GetNum();
+
+            CalcModFunction.calcMod(numInput);
+
+            // Added to leave console open even if started without debugging
+            Console.ReadLine();
+        }
+    }
+}
